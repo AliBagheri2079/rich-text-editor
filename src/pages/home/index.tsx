fb0954de2ac971918f1f4f37a20f6e231e8134cb
Ali Bagheri <alibagheri20ab79@gmail.com>
2024-06-02 17:54:31 +0330

    ✨ feat(app): add github repository link on home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import { useDebounceFn } from 'ahooks';
+import { Image, Typography } from 'antd';
 import { Helmet } from 'react-helmet-async';
 
 import { RichTextEditor } from '@/components';
@@ -19,17 +20,30 @@ export default function Home() {
         <link rel='canonical' href='https://www.tacobell.com/' />
       </Helmet>
 
-      <div className='mx-40 my-60'>
-        <RichTextEditor
-          placeholder='توضیحات را اینجا وارد کنید'
-          options={{
-            content: description,
-            onUpdate: ({ editor }) => {
-              updateDescription(editor.getHTML());
-            },
-          }}
-        />
-      </div>
+      <main>
+        <div className='mx-40 my-48'>
+          <RichTextEditor
+            placeholder='توضیحات را اینجا وارد کنید'
+            options={{
+              content: description,
+              onUpdate: ({ editor }) => {
+                updateDescription(editor.getHTML());
+              },
+            }}
+          />
+        </div>
+
+        <div className='absolute bottom-0 left-0 m-5'>
+          <Typography.Link href='https://github.com/AliBagheri2079/rich-text-editor'>
+            <Image
+              src='https://cdn-icons-png.flaticon.com/512/25/25231.png'
+              width={40}
+              height={40}
+              preview={false}
+            />
+          </Typography.Link>
+        </div>
+      </main>
     </>
   );
 }
